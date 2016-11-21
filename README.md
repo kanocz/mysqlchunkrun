5e61fe9450ba42b6d2671971a6ec01ce2583c02b
Anton Skorokhod <anton@nsl.cz>
2016-11-21 02:41:27 +0100

    Add replication control

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # mysqlchunkrun
-Small tool to run DELETE or UPDATE queries in small portions (with LIMIT)
+Small tool to run DELETE or UPDATE queries in small portions (with LIMIT) and controls replication lag
 
 # usage
 something like this:
 ```
 mysqlchunkrun \
-    -host "localhost" \
-    -user "user" \
-    -password "mysqlpassword" \
-    -database "stats" \
+    -master 'root:password@tcp(masterhost:3306)/stats?charset=utf8mb4' \
+    -slave 'root:password@tcp(slavehost:3306)/?charset=utf8mb4' \
+    -maxlag 5 \
     "DELETE FROM stats5 WHERE ts < '2016-01-01 00:00:00' LIMIT 100000"
 ```
 
-this will run `DELETE FROM stats5 WHERE ts < '2016-01-01 00:00:00' LIMIT 100000` until no more rows affected
+this will run `DELETE FROM stats5 WHERE ts < '2016-01-01 00:00:00' LIMIT 100000` until no more rows affected, checking that slave is synced with master after each chunk (master_pos_wait + control seconds_behind_master value)
--- main.go
+++ main.go
@@ -2,32 +2,152 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
-	dbHost = flag.String("host", "localhost", "MySQL host to connect")
-	dbPort = flag.Int("port", 3306, "MySQL port to connect")
-	dbUser = flag.String("user", "root", "MySQL user to use")
-	dbPass = flag.String("password", "", "MySQL password to use")
-	dbDb   = flag.String("database", "", "MySQL database to select before run")
+	dbMaster = flag.String("master", "root:@tcp(localhost:3306)/?charset=utf8mb4", "connection string for master")
+	dbSlave  = flag.String("slave", "", "connection string for slave")
+	dbMaxLag = flag.Int("maxlag", 10, "maximum allowed slave lag")
 )
+
+// return current master "replication position"
+func getMasterPost(master *sql.DB) (string, uint64, error) {
+	var (
+		file     string
+		pos      uint64
+		doDb     string
+		ignoreDb string
+		gtid     string
+	)
+	err := master.QueryRow("SHOW MASTER STATUS").Scan(&file, &pos, &doDb, &ignoreDb, &gtid)
+	if nil != err {
+		return "", 0, err
+	}
+	return file, pos, nil
+}
+
+// wait until slave processes all statements up to specified position
+func waitForSlave(slave *sql.DB, file string, post uint64) error {
+	// wait up to 1 hour in loop
+
+	for {
+		var result sql.NullInt64
+		err := slave.QueryRow("SELECT MASTER_POS_WAIT(?, ?, ?)", file, post, 3600).Scan(&result)
+		//		err := slave.QueryRow(fmt.Sprintf("MASTER_POS_WAIT('%s', %d, 3600)", file, post)).Scan(&result)
+		if nil != err {
+			return err
+		}
+		if !result.Valid {
+			return errors.New("Replication error")
+		}
+		if result.Int64 != -1 {
+			return nil
+		}
+	}
+}
+
+func getSlaveLag(slave *sql.DB) (int, error) {
+	var slaveStatus struct {
+		SlaveIOState              *sql.RawBytes
+		MasterHost                *sql.RawBytes
+		MasterUser                *sql.RawBytes
+		MasterPort                *sql.RawBytes
+		ConnectRetry              *sql.RawBytes
+		MasterLogFile             *sql.RawBytes
+		ReadMasterLogPos          *sql.RawBytes
+		RelayLogFile              *sql.RawBytes
+		RelayLogPos               *sql.RawBytes
+		RelayMasterLogFile        *sql.RawBytes
+		SlaveIORunning            string
+		SlaveSQLRunning           string
+		ReplicateDoDB             *sql.RawBytes
+		ReplicateIgnoreDB         *sql.RawBytes
+		ReplicateDoTable          *sql.RawBytes
+		ReplicateIgnoreTable      *sql.RawBytes
+		ReplicateWildDoTable      *sql.RawBytes
+		ReplicateWildIgnoreTable  *sql.RawBytes
+		LastErrno                 *sql.RawBytes
+		LastError                 *sql.RawBytes
+		SkipCounter               *sql.RawBytes
+		ExecMasterLogPos          *sql.RawBytes
+		RelayLogSpace             *sql.RawBytes
+		UntilCondition            *sql.RawBytes
+		UntilLogFile              *sql.RawBytes
+		UntilLogPos               *sql.RawBytes
+		MasterSSLAllowed          *sql.RawBytes
+		MasterSSLCAFile           *sql.RawBytes
+		MasterSSLCAPath           *sql.RawBytes
+		MasterSSLCert             *sql.RawBytes
+		MasterSSLCipher           *sql.RawBytes
+		MasterSSLKey              *sql.RawBytes
+		SecondsBehindMaster       int
+		MasterSSLVerifyServerCert *sql.RawBytes
+		LastIOErrno               *sql.RawBytes
+		LastIOError               *sql.RawBytes
+		LastSQLErrno              *sql.RawBytes
+		LastSQLError              *sql.RawBytes
+		ReplicateIgnoreServerIds  *sql.RawBytes
+		MasterServerID            *sql.RawBytes
+		MasterUUID                *sql.RawBytes
+		MasterInfoFile            *sql.RawBytes
+		SQLDelay                  *sql.RawBytes
+		SQLRemainingDelay         *sql.RawBytes
+		SlaveSQLRunningState      *sql.RawBytes
+		MasterRetryCount          *sql.RawBytes
+		MasterBind                *sql.RawBytes
+		LastIOErrorTimestamp      *sql.RawBytes
+		LastSQLErrorTimestamp     *sql.RawBytes
+		MasterSSLCrl              *sql.RawBytes
+		MasterSSLCrlpath          *sql.RawBytes
+		RetrievedGtidSet          *sql.RawBytes
+		ExecutedGtidSet           *sql.RawBytes
+		AutoPosition              *sql.RawBytes
+	}
+
+	err := slave.QueryRow("SHOW SLAVE STATUS").Scan(&slaveStatus.SlaveIOState, &slaveStatus.MasterHost, &slaveStatus.MasterUser, &slaveStatus.MasterPort, &slaveStatus.ConnectRetry, &slaveStatus.MasterLogFile, &slaveStatus.ReadMasterLogPos, &slaveStatus.RelayLogFile, &slaveStatus.RelayLogPos, &slaveStatus.RelayMasterLogFile, &slaveStatus.SlaveIORunning, &slaveStatus.SlaveSQLRunning, &slaveStatus.ReplicateDoDB, &slaveStatus.ReplicateIgnoreDB, &slaveStatus.ReplicateDoTable, &slaveStatus.ReplicateIgnoreTable, &slaveStatus.ReplicateWildDoTable, &slaveStatus.ReplicateWildIgnoreTable, &slaveStatus.LastErrno, &slaveStatus.LastError, &slaveStatus.SkipCounter, &slaveStatus.ExecMasterLogPos, &slaveStatus.RelayLogSpace, &slaveStatus.UntilCondition, &slaveStatus.UntilLogFile, &slaveStatus.UntilLogPos, &slaveStatus.MasterSSLAllowed, &slaveStatus.MasterSSLCAFile, &slaveStatus.MasterSSLCAPath, &slaveStatus.MasterSSLCert, &slaveStatus.MasterSSLCipher, &slaveStatus.MasterSSLKey, &slaveStatus.SecondsBehindMaster, &slaveStatus.MasterSSLVerifyServerCert, &slaveStatus.LastIOErrno, &slaveStatus.LastIOError, &slaveStatus.LastSQLErrno, &slaveStatus.LastSQLError, &slaveStatus.ReplicateIgnoreServerIds, &slaveStatus.MasterServerID, &slaveStatus.MasterUUID, &slaveStatus.MasterInfoFile, &slaveStatus.SQLDelay, &slaveStatus.SQLRemainingDelay, &slaveStatus.SlaveSQLRunningState, &slaveStatus.MasterRetryCount, &slaveStatus.MasterBind, &slaveStatus.LastIOErrorTimestamp, &slaveStatus.LastSQLErrorTimestamp, &slaveStatus.MasterSSLCrl, &slaveStatus.MasterSSLCrlpath, &slaveStatus.RetrievedGtidSet, &slaveStatus.ExecutedGtidSet, &slaveStatus.AutoPosition)
+
+	if sql.ErrNoRows == err {
+		return 0, errors.New("No slave running")
+	}
+
+	if nil != err {
+		return 0, err
+	}
+
+	if slaveStatus.SlaveIORunning != "Yes" {
+		return 0, errors.New("Slave IO not running")
+	}
+
+	if slaveStatus.SlaveSQLRunning != "Yes" {
+		return 0, errors.New("Slave SQL not running")
+	}
+
+	return slaveStatus.SecondsBehindMaster, nil
+}
 
 func main() {
 
 	flag.Parse()
 
-	database, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true",
-		*dbUser, *dbPass, *dbHost, *dbPort, *dbDb))
-
+	master, err := sql.Open("mysql", *dbMaster)
 	if nil != err {
-		log.Fatalln("Error connecting to database: ", err)
+		log.Fatalln("Error connecting to database (master): ", err)
 	}
+	defer master.Close()
+
+	slave, err := sql.Open("mysql", *dbSlave)
+	if nil != err {
+		log.Fatalln("Error connecting to database (slave): ", err)
+	}
+	defer slave.Close()
 
 	query := flag.Arg(0)
 	if "" == query {
@@ -35,7 +155,7 @@ func main() {
 		return
 	}
 
-	stmt, err := database.Prepare(query)
+	stmt, err := master.Prepare(query)
 	if nil != err {
 		log.Fatalln("Error preparing query: ", err)
 	}
@@ -52,6 +172,27 @@ func main() {
 			}
 			fmt.Println("0 affected rows, probably done")
 			break
+		}
+
+		logFile, logPos, err := getMasterPost(master)
+		if nil != err {
+			log.Fatalln("Error reading master position: ", err)
+		}
+
+		err = waitForSlave(slave, logFile, logPos)
+		if nil != err {
+			log.Fatalln("Error waiting for slave: ", err)
+		}
+
+		for {
+			lag, err := getSlaveLag(slave)
+			if nil != err {
+				log.Fatalln("get lag error: ", err)
+			}
+			if lag <= *dbMaxLag {
+				break
+			}
+			time.Sleep(time.Second)
 		}
 
 		os.Stdout.Write([]byte("."))
